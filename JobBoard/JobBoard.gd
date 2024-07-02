516e49949f24137b0ffce0198cf01901964fb77c
Jacob Foxe <fokkushi@gmail.com>
2024-07-02 16:17:28 -0400

    Added Job Board

---
+++ JobBoard/JobBoard.gd
@@ -0,0 +1,21 @@
+extends StaticBody3D
+
+const flyerNode = preload("res://Jobs/JobFlyer.tscn")
+@onready var pins : Array[Marker3D] = [$Pins/Pin0, $Pins/Pin1, $Pins/Pin2, $Pins/Pin3]
+
+func _ready():
+	populate_job_board()
+	
+func populate_job_board():
+	## Loop through pins and create a flyer for each. 
+	for pin in pins:
+		create_job_flyer(JOB_MANAGER.get_random_job(), pin)
+
+#/
+## Instantiate a new flyer and populate it with a job. 
+func create_job_flyer(job : JOB, pin : Marker3D):
+	var newFlyer : JOB_FLYER = flyerNode.instantiate()
+	pin.add_child(newFlyer)
+	newFlyer.set_Job(job)
+	print("Job Created: " + job.title)
+	
