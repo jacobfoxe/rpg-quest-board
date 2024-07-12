fed880af173cd6bbd91c847c10d08624b1199a49
Jacob Foxe <fokkushi@gmail.com>
2024-07-12 16:16:37 -0400

    Added Player Detection and Interaction to the Job Board

--- JobBoard/JobBoard.gd
+++ JobBoard/JobBoard.gd
@@ -1,7 +1,11 @@
 extends StaticBody3D
 
+signal open_board(job_list)
+
 const flyerNode = preload("res://Jobs/JobFlyer.tscn")
 @onready var pins : Array[Marker3D] = [$Pins/Pin0, $Pins/Pin1, $Pins/Pin2, $Pins/Pin3]
+
+var jobs : Array[JOB] = []	## List of jobs on this board. 
 
 func _ready():
 	populate_job_board()
@@ -19,4 +23,28 @@ func create_job_flyer(job : JOB, pin : Marker3D):
 	var newFlyer : JOB_FLYER = flyerNode.instantiate()
 	pin.add_child(newFlyer)
 	newFlyer.set_Job(job)
+	jobs.push_back(job)
 	print("Job Created: " + job.title + " on pin: " + pin.name)
+
+#/
+## Connected to activePlayer signal for interaction. 
+func _on_player_interact(body : PLAYER):
+	emit_signal("open_board", jobs)
+	print(str(body) + " interacted with me!")
+
+#/
+## Called when a body has entered.
+func _on_player_detection_body_entered(body):
+	## NOTE: Some of this logic could be moved to the PlayerDetection node, 
+	## and the check for the body type could be there. 
+	if body.is_in_group("PLAYER"):
+		print("Player Entered: " + str(body))
+		body.interact.connect(_on_player_interact.bind(body))
+
+#/
+## Called when a body has left.
+func _on_player_detection_body_exited(body):
+	## Check for the PLAYER group. Don't update if a player is already within the range. 
+	if body.is_in_group("PLAYER"):
+		print("Player Exited: "+ str(body))
+		body.interact.disconnect(_on_player_interact)
