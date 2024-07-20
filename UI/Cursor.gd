bf7053249c77e8cc96ba3047ea3521340cdaa3c9
Jacob <fokkushi@gmail.com>
2024-07-19 22:58:44 -0400

    Added Cursor node

---
+++ UI/Cursor.gd
@@ -0,0 +1,27 @@
+## Cursor module.
+class_name Cursor extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_process(false)
+	self.visible = false
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Update the position of the cursor to match the mouse position. 
+	global_position = get_global_mouse_position()
+	
+
+## Activates the cursor and makes it visible on the screen. 
+func activateCursor() -> void: 
+	position = get_global_mouse_position()
+	self.visible = true
+	set_process(true)
+
+## Deactivates the cursor and makes it invisible. 
+func deactivateCursor() -> void: 
+	self.visible = false
+	set_process(false)
